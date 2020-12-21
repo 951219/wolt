0aaa0a3637f1c0902fa102b05db3594de52ebb2f
Kenert Karu <karukenert@gmail.com>
2020-12-21 11:56:53 +0200

    removed bootstrap

---
+++ 2020/index.js
@@ -0,0 +1,23 @@
+fetch("./data/restaurants.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        var restolist = data.restaurants;
+        console.log(restolist);
+        restolist.forEach((element) => {
+            var item = (document.createElement("div").innerHTML = `
+            <div class="container">
+              <img src="${element.image}" alt="Image" style="width:100%;">
+              <div class="text">
+                <h4 class="top-left-text">${element.name}</h4>
+                <h6 class="top-left-text2">${element.description}</h6>
+              </div>
+            </div>`);
+
+            document.getElementById("list").innerHTML += item;
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
