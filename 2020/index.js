7a823d62f247af61b5db691b1a8e66c058551142
Kenert Karu <karukenert@gmail.com>
2020-12-24 00:00:54 +0200

    added dummy function for a sorting div

--- 2020/index.js
+++ 2020/index.js
@@ -1,12 +1,9 @@
-
-
 fetch("./data/restaurants.json")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         var restolist = data.restaurants;
-        console.log(restolist);
         restolist.forEach((element) => {
             var item = (document.createElement("div").innerHTML = `
             <div class="container">
@@ -23,14 +20,3 @@ fetch("./data/restaurants.json")
     .catch((err) => {
         console.log(err);
     });
-
-
-
-// // document.getElementById('sort').onClick = function () { alert('1'); };
-
-// document.getElementById('sort').onclick = function changeContent() {
-
-//     document.getElementById('sort').textContent = "Help me";
-//     document.getElementById('sort').style = "Color: red";
-
-// }
