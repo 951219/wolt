754d66aff6f1c1e9675e1b80c348771d4b6c4bf1
Kenert Karu <karukenert@gmail.com>
2021-01-31 19:36:49 +0200

    everything seems to work

--- 2021/server.js
+++ 2021/server.js
@@ -8,58 +8,118 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
 
-
-
 const restaurants = JSON.parse(fs.readFileSync('./data/restaurants.json')).restaurants;
 
 
 ///discovery?lat=60.1709&lon=24.941
 app.get("/discovery", async (req, res) => {
+
     const latitude = parseFloat(req.query.lat);
     const longitude = parseFloat(req.query.lon);
+    const userLocation = { latitude, longitude };
 
-    let popular = await getPopularRestaurants();
-    let newest = await getNewestRestaurants();
-    let nearby = await getNearbyRestaurants(latitude, longitude);
+    let restaurantsCloseToMeList = await getRestosCloseToMe(userLocation);
 
-    let sections = [{
-        "title": "Popular Restaurants",
-        "restaurants": popular
-    }, {
-        "title": "New Restaurants",
-        "restaurants": newest
-    }, {
-        "title": "Nearby Restaurants",
-        "restaurants": nearby
-    }];
+    let openRestos = restaurantsCloseToMeList.filter(element => element.online === true);
+    let closedRestos = restaurantsCloseToMeList.filter(element => element.online === false);
+
+    let sections = [];
+
+    let popular = await getPopularRestaurants(openRestos, closedRestos);
+    if (popular.length > 0) {
+        if (popular.length > 10) {
+            popular = popular.slice(0, 10);
+        };
+        sections.push({
+            "title": "Popular Restaurants",
+            "restaurants": popular
+        });
+    }
+
+    let newest = await getNewestRestaurants(openRestos, closedRestos);
+    if (newest.length > 0) {
+        if (newest.length > 10) {
+            newest = newest.slice(0, 10);
+        };
+        sections.push({
+            "title": "New Restaurants",
+            "restaurants": newest
+        });
+    }
+
+    let nearby = await getNearbyRestaurants(userLocation, openRestos, closedRestos);
+    if (nearby.length > 0) {
+        if (nearby.length > 10) {
+            nearby = nearby.slice(0, 10);
+        };
+
+        sections.push({
+            "title": "Nearby Restaurants",
+            "restaurants": nearby
+        });
+    };
 
     res.status(200).json({ sections });
 });
 
 
-async function getPopularRestaurants() {
-    let list = await restaurants.sort((a, b) => {
+async function getPopularRestaurants(openRestos, closedRestos) {
+    let popularList = await openRestos.sort((a, b) => {
         if (a.popularity > b.popularity) { return -1 } else { return 1 };
     });
-    return list.slice(0, 10);
+
+
+    if (popularList.length < 10) {
+        console.log('in if');
+        let closedPopularList = await closedRestos.sort((a, b) => {
+            if (a.popularity > b.popularity) { return -1 } else { return 1 };
+        });
+
+        popularList.concat(closedPopularList);
+    }
+
+
+    console.log(popularList.length);
+    return popularList;
 };
 
 // newest
-async function getNewestRestaurants() {
-    let list = await restaurants.sort((a, b) => {
+async function getNewestRestaurants(openRestos, closedRestos) {
+
+    var date = new Date();
+    date.setMonth(date.getMonth() - 4);
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    if (month.length = 1) month = `0${month}`;
+    var day = date.getDate();
+    if (day.length = 1) day = `0${day}`;
+    date = `${year}-${month}-${day}`;
+
+    let newestList = await openRestos.filter(element => element.launch_date > date);
+
+    newestList = await newestList.sort((a, b) => {
         if (a.launch_date > b.launch_date) { return -1 } else { return 1 };
     });
-    // TODO “New Restaurants”: Newest launch_date first (descending). This list has also a special rule: launch_date must be no older than 4 months.
 
-    return list.slice(0, 10);
+    if (newestList.length < 10) {
+        let closedList = await closedRestos.filter(element => element.launch_date > date);
+
+        closedList = await closedRestos.sort((a, b) => {
+            if (a.launch_date > b.launch_date) { return -1 } else { return 1 };
+        });
+
+        newestList.concat(closedList);
+    }
+
+    return newestList;
 };
 
 // nearby
-async function getNearbyRestaurants(lat, lon) {
-    const userLocation = { lat, lon };
-    let list = await restaurants.sort((a, b) => {
-        const firstRestoLocation = { lat: a.location[1], lon: a.location[0] }
-        const secondRestoLocation = { lat: b.location[1], lon: b.location[0] }
+async function getNearbyRestaurants(userLocation, openRestos, closedRestos) {
+
+    let nearbyList = await openRestos.sort((a, b) => {
+        const firstRestoLocation = { latitude: a.location[1], longitude: a.location[0] };
+        const secondRestoLocation = { latitude: b.location[1], longitude: b.location[0] };
 
         const userToFirstResto = geolib.getPreciseDistance(userLocation, firstRestoLocation);
 
@@ -69,17 +129,37 @@ async function getNearbyRestaurants(lat, lon) {
         if (userToFirstResto > userToSecondResto) { return 1 } else { return -1 };
     });
 
-    return list.slice(0, 10);
+    if (nearbyList.length < 10) {
+        let closedNearbyList = await closedRestos.sort((a, b) => {
+            const firstRestoLocation = { latitude: a.location[1], longitude: a.location[0] };
+            const secondRestoLocation = { latitude: b.location[1], longitude: b.location[0] };
 
+            const userToFirstResto = geolib.getPreciseDistance(userLocation, firstRestoLocation);
+
+            const userToSecondResto = geolib.getPreciseDistance(userLocation, secondRestoLocation);
+
+
+            if (userToFirstResto > userToSecondResto) { return 1 } else { return -1 };
+        });
+
+        nearbyList.concat(closedNearbyList);
+
+    }
+
+    return nearbyList;
 };
 
 
-//TODO double check if there are 10 items and if not, add empty objects.
+// 1.5 kilometers check
+async function getRestosCloseToMe(userLocation) {
+    let restaurantsCloseToMeList = [];
+    restaurants.forEach(element => {
+        const distance = geolib.getPreciseDistance(userLocation, { latitude: element.location[1], longitude: element.location[0] });
+        if (distance < 1500) {
+            restaurantsCloseToMeList.push(element);
+        }
+    });
 
-//TODO All restaurants returned by the endpoint must be closer than 1.5 kilometers 
-// from given coordinates, measured as a straight line between coordinates and the location of the restaurant.
-
-
-//TODO Open restaurants (online=true) are more important than closed ones. 
-// Every list must be first populated with open restaurants, and only adding closed ones if there is still capacity left.
+    return restaurantsCloseToMeList;
+}
 
